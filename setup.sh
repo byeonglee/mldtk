b6a73157cfe4368c03c3a279ecca2f89f906c635
Byeongcheol Lee <lineonking@gmail.com>
2021-11-20 23:34:44 +0900

    Add Jinn to the xtc build process

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
-JAVA_DEV_ROOT=~/xtc
-CLASSPATH=~/xtc/classes:~/xtc/bin/junit.jar:~/xtc/bin/javabdd.jar
-JAVA_HOME=/usr/bin
+JAVA_DEV_ROOT=${PWD}
+CLASSPATH=${JAVA_DEV_ROOT}/classes:${JAVA_DEV_ROOT}/bin/junit.jar:${JAVA_DEV_ROOT}/bin/javabdd.jar
+JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
 PATH_SEP=:
-export JAVA_DEV_ROOT CLASSPATH JAVA_HOME PATH_SEP
+export JAVA_DEV_ROOT CLASSPATH JAVA_HOME PATH_SEP OSTYPE
